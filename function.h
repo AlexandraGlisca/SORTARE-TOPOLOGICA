ed603a25e98904c86a5c046ea84b959139b0d835
AlexandraGlisca <39824881+AlexandraGlisca@users.noreply.github.com>
2018-06-05 18:04:17 +0300

    Add files via upload

---
+++ function.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_INCLUDE
+#endif // MAIN_H
+#define MAIN_H_INCLUDED
+
+ #include<stdio.h> ///> printf()
+ #include<stdlib.h>
+ #include<math.h>
+
+ #ifndef DOXY FUNCTIONS_H
+ #define DOXY FUNCTIONS_H
+ void create_graph();
+ void insert_queue(int v);
+ int delete_queue();
+ int isEmpty_queue();
+ int indegree(int v);
+
+#endif // MAIN_H
+
